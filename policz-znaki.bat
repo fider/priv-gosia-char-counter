780003d3f8f8691af7e7302b3383f00f43f10269
Wojciech Fiderek <wfiderek@lenovo.com>
2018-10-13 17:53:24 +0200

    Add setup.bat

--- policz-znaki.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-node C:\Users\wfide\work\char-counter\index.js
-pause
---
+++ setup.bat
@@ -0,0 +1,11 @@
+@echo off
+@echo @echo off> policz-znaki.bat
+@echo node %cd%\index.js>> policz-znaki.bat
+@echo pause>> policz-znaki.bat
+
+echo Gotowe, stworzono plik "policz-znaki" (jest w folderze %cd%).
+echo Przenies go do folderu z plikiem do policzenia i odpal.
+echo.
+echo. 
+
+pause
